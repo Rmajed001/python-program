a230be31ccb97953496e1b46632045f3e8478b95
Ruhul Amin <ruhulamin5115h@gmail.com>
2025-06-11 22:34:13 +0600

    added more data

--- SecondProgram.py
+++ SecondProgram.py
@@ -1,1 +1,25 @@
 #Variables: A variable is a name given to a memory location in a program .
+
+name ="Md. Ruhul Amin"
+age = 23
+price = 25.99 
+
+print(name)
+print("Age is:", age)
+print("Price is:",price)
+
+
+
+print(type(name)) #type is string
+print(type(age))  #type is integer      
+print(type(price)) #type is float
+# output is:
+# <class 'str'>
+# <class 'int'>
+# <class 'float'>
+
+ Data Types in Python:
+     Integers: Whole numbers, positive or negative, without decimals.
+     Floats: Numbers with decimal points.
+        Strings: Text data enclosed in quotes (single or double) ('')/("").
+    Boolean: Represents True or False values.
