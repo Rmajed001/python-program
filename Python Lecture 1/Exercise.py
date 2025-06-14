84aebd4edaa4c6fa8ec2ad7b5b2833687426bbe3
Ruhul Amin <ruhulamin5115h@gmail.com>
2025-06-14 23:25:12 +0600

    finished lecture 1

---
+++ Python Lecture 1/Exercise.py
@@ -0,0 +1,22 @@
+
+
+first=int (input("enter first : "))
+second=int(input("enter second : "))
+print("sum=", first + second)
+
+#write a program(WAP) to input side of square & print its area.
+
+side =float(input("enter square side : ")) 
+print("area =", side ** 2)
+
+## write a program(WAP) to input  two floating point numbers and print their avarage.
+
+a=float(input("enter first number : "))
+b=float(input("enter second number : "))
+print("average =", (a + b) / 2)
+
+#Write a program(WAP) to input two numbers a and b. Print True if a is greater than or equal to b, if not print False.
+
+a=int(input("enter first number : "))
+b=int(input("enter second number : "))
+print(a >= b)
